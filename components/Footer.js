27897cfa9ab5f53e4ded11e4c1ffadde9cf0c4a0
DESKTOP-BTL2IC3\jokin <jokinha95@gmail.com>
2022-09-25 14:28:46 -0300

    editando documentação

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import style from '../styles/Footer.module.css'
 export default function Footer() {
     return <footer className={style.footer}>
         <div>Projeto de estudo Next.js @2022</div>
-        <div><a href='https://github.com/ChristpherFeilstrecker/Chuck-Norris-Fatos-Next.js-' target="_blank" rel='noreferrer'>Documentação</a></div>
+        <div><a href='https://github.com/ChristpherFeilstrecker/ChuckNorrisFacts_Nextjs' target="_blank" rel='noreferrer'>Documentação</a></div>
         <div> Developer <a href='https://www.linkedin.com/in/christopher-feilstrecker/' target="_blank" rel='noreferrer'>@Christopher F. da Silva</a></div>
     </footer>
 }
